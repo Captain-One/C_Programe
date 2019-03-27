4a8bce1637ee304a9562a1f3e422e566c5b01688
Captain-One <pxqjoe@sina.com>
2019-03-27 17:35:11 +0800

    添加boot参数表解码部分，部分逻辑有待验证

--- DSP/6678/SBL/main.c
+++ DSP/6678/SBL/main.c
@@ -3,6 +3,123 @@
 #include <string.h>
 #include <ti/platform/platform.h>
 
+#define MAGIC_ADDR                      0x0087FFFC
+#define BIN_FILE_START_ADDR             0x4000
+#define NAND_BUS_WIDTH                  8
+#define NAND_PAGE_SIZE                  512
+#define NAND_ONE_BLOCK_PAGE_COUNT       32
+#define NAND_BLOCK_SIZE                 NAND_PAGE_SIZE*NAND_ONE_BLOCK_PAGE_COUNT
+
+#define X32(x)                          (((x) & 0x000000FF) << 24) | (((x) & 0x0000FF00) << 8) |\
+                                        (((x) & 0x00FF0000) >> 8) | (((x) & 0xFF000000) >> 24)
+
+typedef struct boottble{
+    uint32_t    len;
+    uint32_t    addr;
+    uint8_t     data[0];
+}Boot_table_s;
+
+
+int Boot_Table_Parse(uint8_t *data_buf, uint32_t data_size)
+{
+    Boot_table_s *boot_table;
+
+    boot_table = (Boot_table_s *)(data_buf);
+    boot_table->len = X32(boot_table->len);
+    boot_table->addr = X32(boot_table->addr);
+
+    if(boot_table->len == 0){
+        printf("read boot table over\n");
+        return 0;
+    }
+
+    data_buf += 4;
+
+    if((boot_table->len) < data_size)
+    {
+        memcpy((void *)boot_table->addr, (void *)data_buf, boot_table->len);
+        data_buf += boot_table->len;
+        data_size -= boot_table->len;
+        if(Boot_Table_Parse(data_buf, data_size) == 0){
+            return 0;
+        }
+    }else{
+        memcpy((void *)boot_table->addr, (void *)data_buf, data_size);
+        return 0;
+    }
+}
+
+int Data_Parse(PLATFORM_DEVHANDLE handle)
+{
+    uint8_t *nand_data_buf;
+    uint32_t c_int00_addr;
+    Platform_STATUS status;
+    uint32_t offset = 0;
+    Boot_table_s  *boot_table;
+    uint32_t remain_block_count;
+    uint32_t remian_data_size;
+
+    nand_data_buf = (uint8_t *)malloc(NAND_BLOCK_SIZE);
+    if(nand_data_buf == NULL){
+        printf("malloc nand_data_buf error\n");
+        return -1;
+    }
+
+    status = platform_device_read(handle, BIN_FILE_START_ADDR + offset, nand_data_buf, NAND_BLOCK_SIZE);
+    if(status != Platform_EOK){
+       printf("platform_device_read Error: %d\n", status);
+       return -1;
+    }
+
+    c_int00_addr = X32(*(uint32_t *)nand_data_buf);
+
+    boot_table = (Boot_table_s *)(nand_data_buf + 4);
+    boot_table->len = X32(boot_table->len);
+    boot_table->addr = X32(boot_table->addr);
+
+    remian_data_size = NAND_BLOCK_SIZE -12;
+
+    if(boot_table->len < remian_data_size)
+    {
+        x
+
+        boot_table = (Boot_table_s *)(nand_data_buf + boot_table->len);
+        boot_table->len = X32(boot_table->len);
+        boot_table->addr = X32(boot_table->addr);
+    }
+
+    if(boot_table->len > (NAND_BLOCK_SIZE - 12)){
+        memcpy((void *)boot_table->addr, (void *)(nand_data_buf + 12), NAND_BLOCK_SIZE - 12);
+
+        while(remain_block_count){
+            status = platform_device_read(handle, BIN_FILE_START_ADDR + NAND_BLOCK_SIZE, nand_data_buf, NAND_BLOCK_SIZE);
+            if(status != Platform_EOK){
+               printf("platform_device_read Error: %d\n", status);
+               return -1;
+            }
+
+            remain_block_count --;
+        }
+
+        memcpy((void *)boot_table->addr + NAND_BLOCK_SIZE - 12, (void *)nand_data_buf, NAND_BLOCK_SIZE - 12);
+    }else{
+
+    }
+
+    remain_block_count = 1;
+    if((boot_table->len + 12)/NAND_BLOCK_SIZE > 1){
+        remain_block_count += (boot_table->len + 12)/NAND_BLOCK_SIZE + 1;
+    }
+
+ boot_table = (Boot_table_s *)malloc(sizeof(Boot_table_s) + boot_table->len);
+    if(boot_table == NULL){
+        printf("malloc boot table buf error\n");
+        return -1;
+    }
+
+    return 0;
+}
+
 int main(void)
 {
     Platform_STATUS status;
@@ -11,8 +128,9 @@ int main(void)
     platform_info  platform_info;
     CPU_info  *cpu_info;
     PLATFORM_DEVICE_info *device_info;
-    uint8_t nand_data_buf[1024] = {0};
-    int i;
+    //uint8_t nand_data_buf[1024] = {0};
+    //int i;
+    int re;
 
     memset(&platform_info, 0, sizeof(platform_info));
 
@@ -56,23 +174,20 @@ int main(void)
         printf("Open NAND device Error\n");
         return 0;
     }
-    printf("Open NAND device OK!\n");
+    printf("Open NAND device OK!\nDevice info:\n");
+    printf("    Device ID: %u\n", device_info->device_id);
+    printf("    Manufacturer ID: %u\n", device_info->manufacturer_id);
+    printf("    Width: %u\n", device_info->width);
+    printf("    Block Count: %d\n", device_info->block_count);
+    printf("    Page Count: %d\n", device_info->page_count);
+    printf("    Page Size: %d\n", device_info->page_size);
+    printf("    Spare Size: %d\n", device_info->spare_size);
 
-    status = platform_device_read(device_info->handle, 16384, nand_data_buf, 1024);
-    if(status != Platform_EOK){
-       printf("platform_device_read Error: %d\n", status);
-    }
-    printf("platform_device_read OK!\n");
 
-    for(i = 1; i <= 1024; i++)
-    {
-        printf("0x%2x ",nand_data_buf[i-1]);
-        if(i%16 == 0)
-        {
-            printf("\n");
-        }
+    re = Data_Parse(device_info->handle);
+    if(re < 0){
+        printf("Data_Parse error\n");
     }
-    printf("\n");
 
     status = platform_device_close(device_info->handle);
     if(status != Platform_EOK){
